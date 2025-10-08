f477bc3859aaa92e75f428a47bfaafae25ccee78
Jude Gyimah <jude.gyimah87@gmail.com>
2025-10-08 15:33:32 +0200

    featx_binder refactor

--- featxbinder/launch/early.launch.py
+++ featxbinder/launch/early.launch.py
@@ -1,12 +1,31 @@
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch_ros.actions import Node, ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
 
 def generate_launch_description():
+    pkg_share = get_package_share_directory('featxbinder')
+    yaml_file = os.path.join(pkg_share, 'config', 'featx_params.yaml')
+    print(f"YAML file path: {yaml_file}")
+
+    container = ComposableNodeContainer(
+        name='featx_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container_mt',
+        composable_node_descriptions=[
+            ComposableNode(
+                package='featxbinder',
+                plugin='featx_plugin::FeatxBinder',
+                name='featx_binder',
+                parameters=[yaml_file],
+            ),
+        ],
+        output='screen',
+    )
     return LaunchDescription([
+    container,
     #add all static features to be loaded early here.
-    Node(package='mapping', executable='mapping'),
-    Node(
-         package='featxbinder', 
-         executable='featx_binder', 
-         parameters=['/home/fr0b0/ros2_ws/install/featxbinder/share/featxbinder/config/featx_params.yaml'])
+    Node(package='mapping', executable='mapping')
  ])
--- featxbinder/src/featx_binder.cpp
+++ featxbinder/src/featx_binder.cpp
@@ -1,30 +1,22 @@
 #include "rclcpp/rclcpp.hpp"
 
-class FeatxBinder : public rclcpp::Node{
-public:
-    FeatxBinder() : Node("featx_binder"){
-        this->declare_parameter<std::string>("bindingTime", "Early");
-        rclcpp::Parameter param("bindingTime", "Late");
-        this->set_parameter(param);
-        RCLCPP_INFO(this->get_logger(), "featx_binder running...");
-    }
+namespace featx_plugin{
+    class FeatxBinder : public rclcpp::Node{
+    public:
+        FeatxBinder(const rclcpp::NodeOptions &options) : Node("featx_binder", options){
+            this->declare_parameter<std::string>("bindingTime", "Early");
+            rclcpp::Parameter param("bindingTime", "Late");
+            this->set_parameter(param);
+            RCLCPP_INFO(this->get_logger(), "featx_binder running...");
+        }
 
-    void update_parameter_before_shutdown(){
-        rclcpp::Parameter param("bindingTime", "Early");
-        this->set_parameter(param);
-        RCLCPP_INFO(this->get_logger(), "bindingTime switched to Early before shutdown!");
-    }
-};
-
-int main(int argc, char * argv[]){
-
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<FeatxBinder>();
-
-    rclcpp::spin(node);
-
-    node->update_parameter_before_shutdown();
-
-    rclcpp::shutdown();
-    return 0;
+        void update_parameter_before_shutdown(){
+            rclcpp::Parameter param("bindingTime", "Early");
+            this->set_parameter(param);
+            RCLCPP_INFO(this->get_logger(), "bindingTime switched to Early before shutdown!");
+        }
+    };
 }
+
+#include "rclcpp_components/register_node_macro.hpp"
+RCLCPP_COMPONENTS_REGISTER_NODE(featx_plugin::FeatxBinder)
